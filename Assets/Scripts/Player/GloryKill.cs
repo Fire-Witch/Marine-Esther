bdca48e8dc74ac90b69ab09d68c7501fcd875da8
Fire Witch <74001300+Fire-Witch@users.noreply.github.com>
2022-01-02 02:13:10 +0100

    fix glory kills

--- Assets/Scripts/Player/GloryKill.cs
+++ Assets/Scripts/Player/GloryKill.cs
@@ -7,36 +7,47 @@ public class GloryKill : MonoBehaviour
     public string EnemyName;
 
     public Animator gloryKills;
+    public GameObject gun;
 
     private bool shouldGloryKill = false;
+
+    private GameObject enemy; 
 
     void Start()
     {
         gloryKills = GameObject.Find("GloryKills").GetComponent<Animator>();
+        gun = GameObject.Find("HUDGun");
     }
 
     void Update()
     {
-        if(shouldGloryKill && Input.GetKey("e"))
+       if (shouldGloryKill && Input.GetKey("e"))
         {
+            gun.SetActive(false);
             gloryKills.SetTrigger("KillTurret");
-            Destroy(gameObject);
+            Destroy(enemy);
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Enemy")
         {
             shouldGloryKill = true;
+            enemy = other.transform.parent.gameObject;
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Enemy")
         {
             shouldGloryKill = false;
         }
+    }
+
+    public void RepopGun()
+    {
+        gun.SetActive(true);
     }
 }
